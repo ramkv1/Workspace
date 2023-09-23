51f839fbced27309839ef4f6d2592404803135c9
ramkv1 <93151227+ramkv1@users.noreply.github.com>
2023-09-23 19:55:59 +0530

    Update ATestApplicationRunner.java

--- DataJPA/Boot-proj14-Runners/src/main/java/com/rk/runners/ATestApplicationRunner.java
+++ DataJPA/Boot-proj14-Runners/src/main/java/com/rk/runners/ATestApplicationRunner.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+	
 @Order(1)
 public class ATestApplicationRunner implements ApplicationRunner,Ordered {
 
